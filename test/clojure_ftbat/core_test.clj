de81cfde5a91bea634dbba4572c420c81610a701
Shearerbeard <shearerbeard@gmail.com>
2016-02-21 08:28:00 -0700

    init commit

---
+++ test/clojure_ftbat/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-ftbat.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-ftbat.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
